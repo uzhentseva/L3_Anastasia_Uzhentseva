519f09d9664ff59c894b323c63861ec33a4f92aa
Anastasia Uzhentseva <uzhentseva@yahoo.com>
2018-11-27 20:52:15 +0300

    Initial Commit

---
+++ L3_Anastasia_Uzhentseva/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L3_Anastasia_Uzhentseva
+//
+//  Created by Uzh on 27/11/2018.
+//  Copyright © 2018 Uzh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
